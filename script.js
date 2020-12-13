3dc42b97efb924a2f50b28cdb8aeefeec7cc7b99
khojakapro <azam188003@gmail.com>
2020-12-13 20:05:43 +0500

    nmagapeee

--- script.js
+++ script.js
@@ -4,17 +4,17 @@
 
 let x1 , x , y ,y1 
 do {
-    x1 = x = +prompt('Sonni kriting : ')
-    while(isNaN(x1)){
-        x1 = prompt('Sonni boshqattan kriting :')
+    x1 = +prompt('Sonni kriting : ')
+    while(isNaN(x1) || x1 == 0){
+        x1 = +prompt('Sonni boshqattan kriting :')
     }
     y = +prompt ('Sonning darajasini kriting : ')
-    while(isNaN(y)){
-        y = prompt('Darajani boshqattan kriting : ')
+    while(isNaN(y) || y == 0){
+        y = +prompt('Darajani boshqattan kriting : ')
     }
     
-    
-    for(y1=2;y>=y1;y1++){
+    y1=2
+    for(x=x1;y>=y1;y1++){
     
         x=x*x1
     
